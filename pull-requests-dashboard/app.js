c5d2d0a86528f072fb41bc5d0d7bdd06bde91dff
Eben Bitonte <ebitonte@bloomberg.net>
2016-10-28 13:53:25 -0400

    [EB] basic webhook endpoint

--- pull-requests-dashboard/app.js
+++ pull-requests-dashboard/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var listen = require("./routes/pr");
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use("/listen", listen);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
