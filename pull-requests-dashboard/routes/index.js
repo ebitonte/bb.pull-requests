09a85ce65b352b034af40d59d36e1f3f49c27ee4
Eben Bitonte <ebitonte@bloomberg.net>
2016-10-28 16:23:49 -0400

    [EB] jenkins/github routes, initial view

--- pull-requests-dashboard/routes/index.js
+++ pull-requests-dashboard/routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
+var elasticsearch = require('elasticsearch');
+var client = new elasticsearch.Client({
+  host: 'localhost:9200',
+  log: 'trace'
+});
+
+var ONE_DAY = 1000 * 60 * 60 * 24;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    client.search({
+        index: "pullrequests",
+        type: "pullrequest",
+        q: "*:*"
+    }).then(function(body) {
+        return body.hits.hits.map(function(pr) {
+            const prInfo = pr._source;
+            prInfo.repo = /\/Consumer-Web\/(.+)\/pull/.exec(prInfo.link)[1];
+            prInfo.visibleTime = new Date(Date.parse(prInfo.createdAt)).toLocaleTimeString();
+            prInfo.status = Date.parse(prInfo.updatedAt) - new Date() > 2 * ONE_DAY ? "old" : "new";
+
+            return prInfo;
+        });
+    }).then(function(prs) {
+        res.render("index", {"prs": prs});
+    }).catch(function(error) {
+        res.status(500).send(error);
+    });
 });
 
 module.exports = router;
--- pull-requests-dashboard/routes/pr.js
+++ pull-requests-dashboard/routes/pr.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var bodyParser = require("body-parser");
+
+var jsonParser = bodyParser.json();
 var router = express.Router();
 
 var elasticsearch = require('elasticsearch');
@@ -8,12 +11,13 @@ var client = new elasticsearch.Client({
 });
 var md5 = require("md5");
 
-router.post("/", function(req, res, next) {
+router.post("/jenkins", jsonParser, function(req, res, next) {
     console.log(req.body);
-    var payload = JSON.parse(request.body);
+    var payload = req.body;
     var id = md5(payload.link);
-    payload.createdAt = new Date().getTime() / 1000;
-    payload.updatedAt = new Date().getTime()/1000;
+    var now = new Date();
+    payload.createdAt = now.toISOString();
+    payload.updatedAt = now.toISOString();
 
     client.index({
       index: 'pullrequests',
@@ -27,8 +31,36 @@ router.post("/", function(req, res, next) {
             res.send(200);
         }
     });
+});
 
+router.post("/github", jsonParser, function(req, res, next) {
+    var payload = req.body;
+    var id = md5(payload.link);
+    var now = new Date();
+    payload.createdAt = now.toISOString();
+    payload.updatedAt = now.toISOString();
 
+    var payload = {
+        createdAt: req.body.created_at,
+        updatedAt: req.body.updated_at,
+        link: req.body.pull_request.html_url,
+        author: req.body.user.login,
+        title: req.body.title,
+        body: req.body.body
+    }
+
+    client.index({
+      index: 'pullrequests',
+      type: 'pullrequest',
+      id: id,
+      body: payload
+    }, function (error, response) {
+        if (error) {
+            res.send(500);
+        } else {
+            res.send(200);
+        }
+    });
 });
 
 module.exports = router;
